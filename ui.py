460b1352edc7d235098c85c263c8b6627b871842
capta1nseal <capta1nseal@psil.fi>
2023-01-09 15:25:27 +0200

    rename dict keys

--- ui.py
+++ ui.py
@@ -8,10 +8,10 @@ class UI:
         pygame.init()
 
         self.logic = logic
-        self.scale_constants = {
+        self.window_constants = {
             "window_size": window_size,
-            "scaled_size": (0, 0),
-            "inset_position": (0,0)
+            "scaled_game_size": (0, 0),
+            "inset_game_position": (0,0)
         }
         self.colours = {
             "background_colour": 0x000000,
@@ -23,7 +23,7 @@ class UI:
             "font_colour": 0x000000
         }
 
-        self.screen = pygame.display.set_mode(self.scale_constants["window_size"], pygame.RESIZABLE)
+        self.screen = pygame.display.set_mode(self.window_constants["window_size"], pygame.RESIZABLE)
         self.screen.fill(self.colours["background_colour"])
 
         self.calculate_scaling()
@@ -40,24 +40,24 @@ class UI:
         at the right scale in the right position in the window
         '''
         grid_size = self.logic.get_size()
-        window_size = self.scale_constants["window_size"]
+        window_size = self.window_constants["window_size"]
         if window_size[0] / grid_size[0] <= \
                 window_size[1] / grid_size[1]:
-            self.scale_constants["scaled_size"] = (
+            self.window_constants["scaled_game_size"] = (
                 window_size[0],
                 window_size[0] * grid_size[1] / grid_size[0])
         else:
-            self.scale_constants["scaled_size"] = (
+            self.window_constants["scaled_game_size"] = (
                 window_size[1] * grid_size[0] / grid_size[1],
                 window_size[1])
 
-        self.scale_constants["inset_position"] = (
-            window_size[0] / 2 - self.scale_constants["scaled_size"][0] / 2,
-            window_size[1] / 2 - self.scale_constants["scaled_size"][1] / 2)
+        self.window_constants["inset_game_position"] = (
+            window_size[0] / 2 - self.window_constants["scaled_game_size"][0] / 2,
+            window_size[1] / 2 - self.window_constants["scaled_game_size"][1] / 2)
 
     def render_numbers(self) -> None:
         '''render the numbers to surfaces at scale'''
-        square_size = self.scale_constants["scaled_size"][1] / self.logic.get_size()[1]
+        square_size = self.window_constants["scaled_game_size"][1] / self.logic.get_size()[1]
         font_size = int(square_size * 0.9)
         font = pygame.font.Font("./data/font/Lato/Lato-Bold.ttf", font_size)
         self.numbers = [
@@ -78,7 +78,7 @@ class UI:
                 (0, 0), pygame.RESIZABLE | pygame.FULLSCREEN)
             self.fullscreen = True
 
-        self.scale_constants["window_size"] = self.screen.get_size()
+        self.window_constants["window_size"] = self.screen.get_size()
         self.calculate_scaling()
         self.render_numbers()
 
@@ -95,49 +95,49 @@ class UI:
                 self.logic.stop()
 
             elif event.type == pygame.VIDEORESIZE:
-                self.scale_constants["window_size"] = event.size
+                self.window_constants["window_size"] = event.size
                 self.calculate_scaling()
                 self.render_numbers()
 
                 if self.fullscreen:
                     self.screen = pygame.display.set_mode(
-                        self.scale_constants["window_size"], pygame.RESIZABLE | pygame.FULLSCREEN)
+                        self.window_constants["window_size"], pygame.RESIZABLE | pygame.FULLSCREEN)
                 else:
                     self.screen = pygame.display.set_mode(
-                        self.scale_constants["window_size"], pygame.RESIZABLE)
+                        self.window_constants["window_size"], pygame.RESIZABLE)
                 self.screen.fill(self.colours["background_colour"])
                 pygame.display.update()
 
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_position = pygame.mouse.get_pos()
-                inset_position = self.scale_constants["inset_position"]
-                scaled_size = self.scale_constants["scaled_size"]
+                inset_game_position = self.window_constants["inset_game_position"]
+                scaled_game_size = self.window_constants["scaled_game_size"]
                 field_size = self.logic.get_size()
                 # - event.button: 1=left click, 3=right click
                 if event.button == 1:
-                    if mouse_position[0] >= inset_position[0] and \
-                            mouse_position[0] < inset_position[0]+scaled_size[0] and \
-                            mouse_position[1] >= inset_position[1] and \
-                            mouse_position[1] < inset_position[1]+scaled_size[1]:
+                    if mouse_position[0] >= inset_game_position[0] and \
+                            mouse_position[0] < inset_game_position[0]+scaled_game_size[0] and \
+                            mouse_position[1] >= inset_game_position[1] and \
+                            mouse_position[1] < inset_game_position[1]+scaled_game_size[1]:
                         self.logic.dig(
-                            (int((mouse_position[0] - inset_position[0]) /
-                                (scaled_size[0] / field_size[0])),
-                            int((mouse_position[1] - inset_position[1]) /
-                                (scaled_size[1] / field_size[1]))))
+                            (int((mouse_position[0] - inset_game_position[0]) /
+                                (scaled_game_size[0] / field_size[0])),
+                            int((mouse_position[1] - inset_game_position[1]) /
+                                (scaled_game_size[1] / field_size[1]))))
                     else:
                         print(
                             f"left click outside of game area, coordinates \
                             ({mouse_position[0]},{mouse_position[1]})")
                 elif event.button == 3:
-                    if mouse_position[0] >= inset_position[0] and \
-                            mouse_position[0] < inset_position[0]+scaled_size[0] and \
-                            mouse_position[1] >= inset_position[1] and \
-                            mouse_position[1] < inset_position[1]+scaled_size[1]:
+                    if mouse_position[0] >= inset_game_position[0] and \
+                            mouse_position[0] < inset_game_position[0]+scaled_game_size[0] and \
+                            mouse_position[1] >= inset_game_position[1] and \
+                            mouse_position[1] < inset_game_position[1]+scaled_game_size[1]:
                         self.logic.flag((
-                            int((mouse_position[0] - inset_position[0]) /
-                                (scaled_size[0] / field_size[0])),
-                            int((mouse_position[1] - inset_position[1]) /
-                                (scaled_size[1] / field_size[1]))))
+                            int((mouse_position[0] - inset_game_position[0]) /
+                                (scaled_game_size[0] / field_size[0])),
+                            int((mouse_position[1] - inset_game_position[1]) /
+                                (scaled_game_size[1] / field_size[1]))))
                     else:
                         print(
                             f"right click outside of game area, coordinates \
@@ -166,18 +166,18 @@ class UI:
                         self.pxgrid[x, y] = self.colours["dark_known_colour"] # type: ignore
                     else:
                         self.pxgrid[x, y] = self.colours["light_known_colour"] # type: ignore
-        scaled_size = self.scale_constants["scaled_size"]
-        inset_position = self.scale_constants["inset_position"]
-        square_size = scaled_size[1] / self.logic.get_size()[1]
+        scaled_game_size = self.window_constants["scaled_game_size"]
+        inset_game_position = self.window_constants["inset_game_position"]
+        square_size = scaled_game_size[1] / self.logic.get_size()[1]
         self.screen.blit(pygame.transform.scale(
             self.small_surface,
-            (int(scaled_size[0]), int(scaled_size[1]))),
-            (int(inset_position[0]), int(inset_position[1])))
+            (int(scaled_game_size[0]), int(scaled_game_size[1]))),
+            (int(inset_game_position[0]), int(inset_game_position[1])))
         for x in range(0, size[0]):
             for y in range(0, size[1]):
                 square_value = field[x][y]
                 if square_value not in (0, 10):
                     self.screen.blit(self.numbers[square_value - 1],(
-                        inset_position[0] + x * square_size + self.number_offsets[square_value - 1],
-                        inset_position[1] + y * square_size + self.number_offsets[8]))
+                        inset_game_position[0] + x * square_size + self.number_offsets[square_value - 1],
+                        inset_game_position[1] + y * square_size + self.number_offsets[8]))
         pygame.display.update()
